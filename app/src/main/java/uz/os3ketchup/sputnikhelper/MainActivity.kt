4a24ccad3eecb6533072cdd2e7ea06d97295e8ea
Eldor Teshaboev <eldorteshaboyev1994@gmail.com>
2022-10-20 10:08:58 +0500

    Initial commit

---
+++ app/src/main/java/uz/os3ketchup/sputnikhelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.os3ketchup.sputnikhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
